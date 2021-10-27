4ee170ea89f21f946628c79fa157165c28112430
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-10-27 19:12:14 +0800

    Add VC-LTL support for MileXamlApplication project.

--- MileXamlApplication/pch.h
+++ MileXamlApplication/pch.h
@@ -1,5 +1,15 @@
 ﻿#pragma once
 #include <windows.h>
+
+// https://github.com/microsoft/terminal/blob/c727762602b8bd12e4a3a769053204d7e92b81c5/src/cascadia/WindowsTerminalUniversal/pch.h#L12
+// This is inexplicable, but for whatever reason, cppwinrt conflicts with the
+// SDK definition of this function, so the only fix is to undef it.
+// from WinBase.h
+// Windows::UI::Xaml::Media::Animation::IStoryboard::GetCurrentTime
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+
 #include <unknwn.h>
 #include <restrictederrorinfo.h>
 #include <hstring.h>
