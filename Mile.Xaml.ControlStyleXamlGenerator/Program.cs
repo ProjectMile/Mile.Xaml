c92a56e5d9cb2f39953b2ceb131eeb8cfe05c36a
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-03-17 00:33:14 +0800

    Add Mile.Xaml.ControlStyleXamlGenerator project.

---
+++ Mile.Xaml.ControlStyleXamlGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Mile.Xaml.ControlStyleXamlGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
