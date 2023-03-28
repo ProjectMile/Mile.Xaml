c30d21c9049e5a9b93ad48d3241925230cd1a0b2
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-03-29 00:11:53 +0800

    Remove GetRepositoryRoot method.

--- Mile.Xaml.ControlStyleXamlGenerator/Program.cs
+++ Mile.Xaml.ControlStyleXamlGenerator/Program.cs
@@ -1,43 +1,13 @@
-﻿using System.Diagnostics;
+﻿using Mile.Project.Helpers;
 using System.Xml;
 
 namespace Mile.Xaml.ControlStyleXamlGenerator
 {
     internal class Program
     {
-        public static string GetRepositoryRoot()
-        {
-            Process process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    FileName = "git.exe",
-                    Arguments = "rev-parse --show-toplevel"
-                }
-            };
-
-            if (process.Start())
-            {
-                process.WaitForExit();
-                if (process.ExitCode == 0)
-                {
-                    string? result = process.StandardOutput.ReadLine();
-                    if (result != null)
-                    {
-                        return Path.GetFullPath(result);
-                    }
-                }
-            }
-
-            return string.Empty;
-        }
-
         static void Main(string[] args)
         {
-            string ProjectRootPath = GetRepositoryRoot();
+            string ProjectRootPath = GitRepository.GetRootPath();
             Console.WriteLine(ProjectRootPath);
 
             DirectoryInfo dir = new(ProjectRootPath + @"\Mile.Xaml.Styles.SunValley");
