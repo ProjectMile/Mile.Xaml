ae12ad56c503999de9aa4a3a61a1dc1fe342dbcf
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-19 02:37:43 +0800

    Split XAML control styles binary generation from Mile.Xaml project to Mile.Xaml.Styles project.

---
+++ Mile.Xaml.Styles/Mile.Xaml.Styles.h
@@ -0,0 +1,9 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Xaml.Styles.h
+ * PURPOSE:   Definition for the Mile.Xaml.Styles placeholder
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
