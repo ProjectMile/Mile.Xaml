2654a26671a5d3f5a84484e40a7a1999d46742f5
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-15 08:25:53 +0800

    Add Mile.Xaml.Interop.ICoreWindowInterop.

---
+++ Mile.Xaml.Managed/Interop/ICoreWindowInterop.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      ICoreWindowInterop.cs
+ * PURPOSE:   Definition for Mile.Xaml.Interop.ICoreWindowInterop
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mile.Xaml.Interop
+{
+    [ComImport]
+    [Guid("45D64A29-A63E-4CB6-B498-5781D298CB4F")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ICoreWindowInterop
+    {
+        IntPtr WindowHandle { get; }
+        bool MessageHandled { set; }
+    }
+}
