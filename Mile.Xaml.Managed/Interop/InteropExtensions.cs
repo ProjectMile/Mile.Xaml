2b518da517db840611702f3197df773024a3a9bb
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-15 09:18:59 +0800

    Add ile.Xaml.Interop.InteropExtensions.

---
+++ Mile.Xaml.Managed/Interop/InteropExtensions.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      InteropExtensions.cs
+ * PURPOSE:   Definition for Mile.Xaml.Interop.InteropExtensions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+using System.Runtime.InteropServices;
+using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Hosting;
+
+namespace Mile.Xaml.Interop
+{
+    public static class InteropExtensions
+    {
+        public static IDesktopWindowXamlSourceNative2 GetInterop(
+            this DesktopWindowXamlSource BaseObject)
+        {
+            IntPtr BaseObjectIntPtr = Marshal.GetIUnknownForObject(BaseObject);
+            try
+            {
+                object TypedObject = Marshal.GetTypedObjectForIUnknown(
+                    BaseObjectIntPtr,
+                    typeof(IDesktopWindowXamlSourceNative2));
+                return (IDesktopWindowXamlSourceNative2)TypedObject;
+            }
+            finally
+            {
+                Marshal.Release(BaseObjectIntPtr);
+            }
+        }
+
+        public static ICoreWindowInterop GetInterop(
+            this CoreWindow BaseObject)
+        {
+            IntPtr BaseObjectIntPtr = Marshal.GetIUnknownForObject(BaseObject);
+            try
+            {
+                object TypedObject = Marshal.GetTypedObjectForIUnknown(
+                    BaseObjectIntPtr,
+                    typeof(ICoreWindowInterop));
+                return (ICoreWindowInterop)TypedObject;
+            }
+            finally
+            {
+                Marshal.Release(BaseObjectIntPtr);
+            }
+        }
+    }
+}
