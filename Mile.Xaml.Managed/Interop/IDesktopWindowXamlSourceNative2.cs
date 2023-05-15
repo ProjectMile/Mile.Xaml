f4b4b72cebc6d1203029da34c2899538a043b95e
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-05-15 09:11:13 +0800

    Add Mile.Xaml.Interop.IDesktopWindowXamlSourceNative2.

---
+++ Mile.Xaml.Managed/Interop/IDesktopWindowXamlSourceNative2.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      IDesktopWindowXamlSourceNative2.cs
+ * PURPOSE:   Definition for Mile.Xaml.Interop.IDesktopWindowXamlSourceNative2
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mile.Xaml.Interop
+{
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("e3dcd8c7-3057-4692-99c3-7b7720afda31")]
+    public interface IDesktopWindowXamlSourceNative2
+    {
+        void AttachToWindow(IntPtr parentWnd);
+
+        IntPtr WindowHandle { get; }
+
+        bool PreTranslateMessage(ref System.Windows.Forms.Message message);
+    }
+}
