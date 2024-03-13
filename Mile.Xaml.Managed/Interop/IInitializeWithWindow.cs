aa034b55d0de72731f5e69c7c9d7e2bdbcf2a305
高怡飞 <363301617@qq.com>
2024-03-13 20:03:22 +0800

    Add Mile.Xaml.Interop.IInitializeWithWindow (#25)
    Add Mile.Xaml.Interop.IInitializeWithWindow

---
+++ Mile.Xaml.Managed/Interop/IInitializeWithWindow.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      IInitializeWithWindow.cs
+ * PURPOSE:   Definition for Mile.Xaml.Interop.IInitializeWithWindow
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Mile.Xaml.Interop
+{
+    [ComImport]
+    [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInitializeWithWindow
+    {
+        void Initialize(IntPtr hwnd);
+    }
+}
